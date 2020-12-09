5507247a5d913279ebd015c7f8950f10df933432
Pablo Gonzalez <pabgon0@gmail.com>
2020-12-09 20:30:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign(driver, {[key]: value})
 }
+
+function deleteFromDriverByKey(driver, key) {
+  driver.key.delete()
+  return driver
+}
